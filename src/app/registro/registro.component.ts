ef213d0810bc540f66c2eb9bdc67a81a30008330
Gonzalo Pouget <aryamdelsolar@outlook.com>
2022-07-01 15:43:08 -0300

    EBS-10 Registro basico sin conexion de usuario

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { LoginService } from 'src/app/servicios/login.service';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-
-  constructor() { }
+  email!:string;
+  password!:string;
+  constructor(private flashMessages:FlashMessagesService,private router:Router,private loginService:LoginService) { }
 
   ngOnInit(): void {
+    this.loginService.getAuth().subscribe(auth=>{
+      if(auth){
+        this.router.navigate(['/']);
+      }
+    })
+  }
+  registrar(){
+  this.loginService.registrar(this.email,this.password).then(
+    res=>{
+      this.router.navigate(['/']);
+    }
+  ).catch(error=>{
+    this.flashMessages.show("La direccion de Email ya se encuentra Registrada!",{cssClass:'alert-danger',timeout:10000});
+  })
+  }
+  registrarConGoogle(){
+    
   }
 
 }
